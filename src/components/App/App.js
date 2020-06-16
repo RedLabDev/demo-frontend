f985a08c48be07591abe2e0bbcde7bededd41c37
prostofrost <fotofrost@gmail.com>
2020-06-16 06:10:24 +0400

    chore: remove ant.d and React-Router

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,9 @@
 import React from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
-import PageOne from '../PageOne/PageOne';
-import PageTwo from '../PageTwo/PageTwo';
 
-import 'antd/dist/antd.css';
 import './App.css';
 
 function App() {
-  return (
-    // Use HashRouter for routing on static pages
-    <Router>
-      <Switch>
-        <Route exact path="/">
-          <PageOne />
-        </Route>
-        <Route path="/page-two">
-          <PageTwo />
-        </Route>
-      </Switch>
-    </Router>
-  );
+  return <div className="app">App</div>;
 }
 
 export default App;
