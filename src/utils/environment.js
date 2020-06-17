f5f2eb8bf09b3f7a1a28e626d071d51dfdc79cec
prostofrost <fotofrost@gmail.com>
2020-06-17 15:21:48 +0400

    fix: refactor get env

--- src/utils/environment.js
+++ src/utils/environment.js
@@ -1,13 +1,13 @@
-const localhostEnvironment = 'localhost';
-const prodEnvironment = 'production';
-
 const isLocalHost = () =>
   window.location.host.includes('localhost') ||
   window.location.hostname.includes('127.0.0.1');
 
 export const getEnv = () => {
-  if (isLocalHost()) {
-    return process.env.REACT_APP_ENV || localhostEnvironment;
+  if (process.env.REACT_APP_ENV) {
+    return process.env.REACT_APP_ENV;
   }
-  return prodEnvironment;
+  if (isLocalHost()) {
+    return 'localhost';
+  }
+  return 'prod';
 };
