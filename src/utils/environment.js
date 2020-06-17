3f5520ff7def890cb98f62ff1faa6837f058866e
prostofrost <fotofrost@gmail.com>
2020-06-17 09:58:38 +0400

    fix environment undefined

--- src/utils/environment.js
+++ src/utils/environment.js
@@ -1,11 +1,13 @@
 const localhostEnvironment = 'localhost';
+const prodEnvironment = 'production';
 
 const isLocalHost = () =>
-  window.location.host.includes('localhost:3000') ||
+  window.location.host.includes('localhost') ||
   window.location.hostname.includes('127.0.0.1');
 
 export const getEnv = () => {
   if (isLocalHost()) {
     return process.env.REACT_APP_ENV || localhostEnvironment;
   }
+  return prodEnvironment;
 };
